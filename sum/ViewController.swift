8034a0f7a1968d6403c5609b5054bc852604ac73
20161104596 <964312086@qq.com>
2018-09-26 11:07:42 +0800

    1
    1

--- sum/ViewController.swift
+++ sum/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var X: UITextField!
+    
+    @IBOutlet weak var Y: UITextField!
+    
+    @IBOutlet weak var Z: UITextField!
+    
+    @IBAction func text(_ sender: Any) {
+        Z.text = "\(Int(X.text!)! + Int(Y.text!)!)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
